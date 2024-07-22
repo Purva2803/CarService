3f4fe7706c8a799959c5f240824ab88cfd8d5041
Purva Rajyaguru <81159331+Purva2803@users.noreply.github.com>
2024-07-22 11:41:23 +0530

    Update operator.js

--- data/operator.js
+++ data/operator.js
@@ -1,13 +1,13 @@
 exports.operatorData = [
     {
-        operator_id:1,
+      operator_id:1,
       operator_name: 'ServiceOperator0',
       contact_info: 'Contact Info for Operator 0',
       specialization: 'Specialization 0',
       status: true, // or false, depending on availability
     },
     {
-     operator_id:2,
+      operator_id:2,
       operator_name: 'ServiceOperator1',
       contact_info: 'Contact Info for Operator 1',
       specialization: 'Specialization 1',
